b409db7353dc0d2d7995b4da640f00e400fc4041
aleks_o54 <aleksanderolender22@gmail.com>
2026-05-09 00:40:27 +0200

    6: Add User Input (Terminal)

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbox")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -12,7 +18,7 @@ def main():
     client = genai.Client(api_key=api_key)
 
     model = "gemini-2.5-flash"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = args.user_prompt
     response = client.models.generate_content(model=model, contents=contents)
 
     if response.usage_metadata is None:
