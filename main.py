e27113601fa85abea49d2c70c88dae99b4ad2805
aleks_o54 <aleksanderolender22@gmail.com>
2026-05-08 23:52:05 +0200

    3: Gemini API setup

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("API Key not found")
+
+client = genai.Client(api_key=api_key)
+
+model = "gemini-2.5-flash"
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+response = client.models.generate_content(model=model, contents=contents)
+print(response.text)
+
 def main():
     print("Hello from ai-agent!")
-
 
 if __name__ == "__main__":
     main()
