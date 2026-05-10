4d4e4f0829ef24797d0b61d8774d800fd24b4a20
aleks_o54 <aleksanderolender22@gmail.com>
2026-05-10 21:11:58 +0200

    Get Files

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,1 +1,17 @@
 from functions.get_files_info import get_files_info 
+
+debug1 = get_files_info("calculator", ".")
+print("Result for current directory:")
+print(debug1)
+
+debug2 = get_files_info("calculator", "pkg")
+print("Result for 'pkg' directory:")
+print(debug2)
+
+debug3 = get_files_info("calculator", "/bin")
+print("Result for '/bin' directory:")
+print(debug3)
+
+debug4 = get_files_info("calculator", "../")
+print("Result for '../' directory:")
+print(debug4)
