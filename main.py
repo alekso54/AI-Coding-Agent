af24b9bb6bd0906936d01030b11f4bf9594a1559
aleks_o54 <aleksanderolender22@gmail.com>
2026-05-12 21:02:46 +0200

    system prompt - im just a robot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbox")
@@ -21,7 +22,12 @@ def main():
     client = genai.Client(api_key=api_key)
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
+
+    )
 
     if response.usage_metadata is None:
         raise RuntimeError("Gemini API response appears to be malformed")
