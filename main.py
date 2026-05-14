6a2131c0d5eabe2d21882c613c2372871b7c5406
aleks_o54 <aleksanderolender22@gmail.com>
2026-05-14 20:58:54 +0200

    agent loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -22,30 +23,64 @@ def main():
     client = genai.Client(api_key=api_key)
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),
-
-    )
-
-    if response.usage_metadata is None:
-        raise RuntimeError("Gemini API response appears to be malformed")
-
-    X = response.usage_metadata.prompt_token_count
-    Y = response.usage_metadata.candidates_token_count
-
-    if args.verbose == True:
-        print(f"User prompt: {args.user_prompt}\n")
-        print(f"Prompt tokens: {X}")
-        print(f"Response tokens: {Y}\n")
     
-    if not response.function_calls:
-        print("Response:")
-        print(response.text)
-        return
-    
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),
 
+        )
+
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+        if response.usage_metadata is None:
+            raise RuntimeError("Gemini API response appears to be malformed")
+
+        X = response.usage_metadata.prompt_token_count
+        Y = response.usage_metadata.candidates_token_count
+
+        if args.verbose == True:
+            print(f"User prompt: {args.user_prompt}\n")
+            print(f"Prompt tokens: {X}")
+            print(f"Response tokens: {Y}\n")
+        
+        if not response.function_calls:
+            print()
+            print(f'User prompt: "{args.user_prompt}"')
+            print("\nResponse:")
+            print(response.text)
+            return
+        
+        function_responses = []
+
+        if response.function_calls:
+
+            for i in range(0, len(response.function_calls)):
+
+                function_call = response.function_calls[i]
+                function_call_result = call_function(function_call, args.verbose)
+
+                if function_call_result.parts == []:
+                    raise Exception("function call result has an empty '.parts' list")
+                
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception("first item in the list of '.parts' is None")
+                
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception("the response field of the FunctionResponse object is None")
+                
+                function_responses.append(function_call_result.parts[0])
+
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            messages.append(types.Content(role="user", parts=function_responses))
+    
+    print("Max iterations reached without final response")
+    sys.exit(1) 
+    
 
 if __name__ == "__main__":
     main()
